8ebfcc345ddb4005e8d03c98dd7eefd9d48457ef
Mehmet Arslan <marslan.developer@gmail.com>
2023-02-20 01:33:02 +0300

    list function done

---
+++ todo/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+priority_choices=[
+    (1, 'High'),
+    (2, 'Medium'),
+    (3, 'Low'),
+]
+status_choices=[
+    ('c', 'Complated'),
+    ('w', 'waiting'),
+    ('p', 'on progress'),
+]
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    priority = models.SmallIntegerField(choices=priority_choices, default=1)
+    status = models.CharField(max_length=10, choices=status_choices, default='w')
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+
+
+
